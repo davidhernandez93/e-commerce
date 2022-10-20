838276fd0c546d732a155e7a048a518a05444153
David <hernandezmendezd@gmail.com>
2022-10-20 16:56:55 -0300

    implementacion de calculo subtotal

--- js/cart.js
+++ js/cart.js
@@ -1,7 +1,6 @@
 const cartUser = '25801.json';
 let cartContent = [];
 let itemsInCart = JSON.parse(localStorage.getItem("listOfItems"));
-let subTotal = 0;
 
 if(itemsInCart === null){
     itemsInCart = [];
@@ -24,20 +23,21 @@ function showCartContent(){
         tRow.innerHTML =`
             <td><image src='${item.image}' width='75' class='img-fluid'></td>
             <td>${item.name}</td>
-            <td>${conversion(item.currency, item.unitCost)}</td>
-            <td id='count-${item.id}'></td>
-            <td><span class='subSpan' id='sub-${item.id}'>${conversion(item.currency, item.unitCost)}</span></td>
+            <td class='unitCost'>USD ${conversion(item.currency, item.unitCost)}</td>
+            <td class='unitQuantity' id='count-${item.id}'></td>
+            <td><span class='subSpan' id='sub-${item.id}'>USD ${conversion(item.currency, item.unitCost)}</span></td>
             `
         document.getElementById('tableBody').appendChild(tRow);
         
         let inputCount = document.createElement('input');
-        inputCount.setAttribute(`class`, `form-control`);
+        inputCount.setAttribute(`class`, `form-control inputQuantity`);
         inputCount.setAttribute(`type`, `number`);
         inputCount.setAttribute(`min`, `1`);
         inputCount.setAttribute(`value`, item.count);
         
         inputCount.addEventListener('input', function(){
-            document.getElementById('sub-'+item.id).innerHTML = calcSubtotal(item.currency, item.unitCost, inputCount.value);
+            document.getElementById('sub-'+item.id).innerHTML = 'USD ' + calcSubtotal(item.currency, item.unitCost, inputCount.value);
+            showSubtotal()
             
         })
         document.getElementById(`count-${item.id}`).appendChild(inputCount);
@@ -73,8 +73,8 @@ function conversion (currency, cost){
         currency='USD';
         cost = Math.round(cost / 40);
     }
-    
-    return currency +' '+ cost
+    let unitCost = Math.round(cost);    
+    return unitCost;
 }
     
 function calcSubtotal(currency, cost, input){
@@ -82,16 +82,20 @@ function calcSubtotal(currency, cost, input){
         currency = 'USD';
         cost = Math.round(cost / 40);
     }
-    return currency + ' ' + cost * input;
+    let prodSubtotal =Math.round(cost * input) ;
+    return prodSubtotal;
 
 }
 
 function showSubtotal(){
-    for(let i = 0; i<cartContent.length; i++){
-        if(cartContent[i].currency==='UYU'){
-            cartContent[i].unitCost = cartContent[i].unitCost / 40;
-        }
-        subTotal += Math.round(cartContent[i].unitCost);
+    let itemRows = document.getElementsByClassName('tRow');
+    let subTotal = 0;
+    for(let i = 0; i<itemRows.length; i++){
+        let itemRow = itemRows[i];
+        let price = parseFloat(itemRow.getElementsByClassName('unitCost')[0].innerText.replace('USD ', ''));
+        let quantity = parseFloat(itemRow.getElementsByClassName('inputQuantity')[0].value);
+        subTotal += (price * quantity);
+
     }
     let divSubtotal = document.getElementById('divSubtotal');
     divSubtotal.innerHTML = `<strong>Total de la compra: USD ${subTotal}</strong>`;
